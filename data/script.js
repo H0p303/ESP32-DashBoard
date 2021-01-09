efd4472581d2f074fbaf5c48e771eb807aab2551
H0p303 <45872829+H0p303@users.noreply.github.com>
2021-01-09 12:22:42 -0800

    Sliders now work

--- data/script.js
+++ data/script.js
@@ -1,7 +1,25 @@
-let root = document.documentElement;
+// let root = document.documentElement;
 
-root.addEventListener("mousemove", e => {
-    if (e.clientY < 101) {
-        root.style.setProperty('--humid-height', e.clientY);
-    }
-});
+// root.addEventListener("mousemove", e => {
+//     if (e.clientY < 101) {
+//         root.style.setProperty('--humid-height', e.clientY);
+//     }
+// });
+
+var red_slider = document.getElementById('red_slider'),
+    red_slider_value = document.getElementById('red_slider-value');
+red_slider.oninput = function () {
+    red_slider_value.innerHTML = this.value;
+}
+
+var green_slider = document.getElementById('green_slider'),
+    green_slider_value = document.getElementById('green_slider-value');
+green_slider.oninput = function () {
+    green_slider_value.innerHTML = this.value;
+}
+
+var blue_slider = document.getElementById('blue_slider'),
+    blue_slider_value = document.getElementById('blue_slider-value');
+blue_slider.oninput = function () {
+    blue_slider_value.innerHTML = this.value;
+}
