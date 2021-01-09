247bc649628fc2948487aaf6dad6af7f5348792d
H0p303 <45872829+H0p303@users.noreply.github.com>
2021-01-09 12:39:25 -0800

    sliders have functionality

--- data/script.js
+++ data/script.js
@@ -1,4 +1,4 @@
-// let root = document.documentElement;
+let root = document.documentElement;
 
 // root.addEventListener("mousemove", e => {
 //     if (e.clientY < 101) {
@@ -10,16 +10,19 @@ var red_slider = document.getElementById('red_slider'),
     red_slider_value = document.getElementById('red_slider-value');
 red_slider.oninput = function () {
     red_slider_value.innerHTML = this.value;
+    root.style.setProperty('--red', this.value);
 }
 
 var green_slider = document.getElementById('green_slider'),
     green_slider_value = document.getElementById('green_slider-value');
 green_slider.oninput = function () {
     green_slider_value.innerHTML = this.value;
+    root.style.setProperty('--green', this.value);
 }
 
 var blue_slider = document.getElementById('blue_slider'),
     blue_slider_value = document.getElementById('blue_slider-value');
 blue_slider.oninput = function () {
     blue_slider_value.innerHTML = this.value;
+    root.style.setProperty('--blue', this.value);
 }
