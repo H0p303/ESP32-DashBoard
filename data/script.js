e4f0beec717fc0dcf4e1cdc0248ec181cf938e4d
H0p303 <45872829+H0p303@users.noreply.github.com>
2021-04-25 15:46:21 -0700

    small changes

--- data/script.js
+++ data/script.js
@@ -27,19 +27,19 @@ var red_slider = document.getElementById('red_slider'),
     red_slider_value = document.getElementById('red_slider-value');
 red_slider.oninput = function () {
     red_slider_value.innerHTML = this.value;
-    root.style.setProperty('--red', this.value);
+    root.style.setProperty('--R_Slide', this.value);
 }
 
 var green_slider = document.getElementById('green_slider'),
     green_slider_value = document.getElementById('green_slider-value');
 green_slider.oninput = function () {
     green_slider_value.innerHTML = this.value;
-    root.style.setProperty('--green', this.value);
+    root.style.setProperty('--G_Slide', this.value);
 }
 
 var blue_slider = document.getElementById('blue_slider'),
     blue_slider_value = document.getElementById('blue_slider-value');
 blue_slider.oninput = function () {
     blue_slider_value.innerHTML = this.value;
-    root.style.setProperty('--blue', this.value);
+    root.style.setProperty('--B_Slide', this.value);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,11 @@
 #include <Arduino.h>
-#include <AsyncJson.h>
-#include <ESPAsyncWebServer.h>
-#include <ESPmDNS.h>
-#include <SPIFFS.h>
 #include <wifi-credentials.h>
-
-AsyncWebServer server(80);
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED){
-    Serial.printf("Connection Failed... Re-Booting \n");
-    ESP.restart();
-  }
-  Serial.printf("Connected to Network");
-  
-  SPIFFS.begin();
-
-  MDNS.begin("demo-server");
-
-  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");           //Allows acces from any websites
-  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "GET, PUT");   //Using GET and PUT methods
-  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "*");          //With any headers
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
